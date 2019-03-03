2abcbb6a4ced75caec83c282b31295d72afd3ab8
Electro Mysterio <raydiati0n@hotmail.com>
2019-03-04 03:14:24 +0600

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,41 @@
+import { GooglePlus } from '@ionic-native/google-plus/ngx';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import * as firebase from 'firebase';
+import { AngularFireModule } from 'angularfire2'
+import { AngularFireAuthModule } from 'angularfire2/auth'
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyCmJN9aeUk2OccxSUaLcJwSP34oqmqjfkE",
+    authDomain: "medalert-fd67a.firebaseapp.com",
+    databaseURL: "https://medalert-fd67a.firebaseio.com",
+    projectId: "medalert-fd67a",
+    storageBucket: "medalert-fd67a.appspot.com",
+    messagingSenderId: "690959688562"
+}
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+      BrowserModule,
+      IonicModule.forRoot(),
+      AppRoutingModule,
+      AngularFireModule.initializeApp(firebaseConfig),
+      AngularFireAuthModule,
+    ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
+    GooglePlus
   ],
   bootstrap: [AppComponent]
 })
