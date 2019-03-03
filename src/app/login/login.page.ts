579fb20f95cd200c04a37469fe70389d896c55de
Electro Mysterio <raydiati0n@hotmail.com>
2019-03-04 05:10:12 +0600

    removeplatfrom

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -2,6 +2,7 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import { Component, OnInit } from '@angular/core';
 import { GooglePlus } from '@ionic-native/google-plus/ngx';
 import  * as firebase from 'firebase'
+import { JsonPipe } from '@angular/common';
 
 
 
@@ -27,19 +28,22 @@ facebookSignIn(){
 
 }
 
-async googleSignIn(): Promise <void>{
-    try{
-        const Gplus = await this.googlePlus.login({
-            'webClientId': '690959688562-067djvtm6sp6f9a75lu64a6fhj7m7bin.apps.googleusercontent.com',
-            'offline' : true,
-            'scopes' : 'profile email'
+googleSignIn(){
+    
+        const provider = new firebase.auth.GoogleAuthProvider();
+        const log = this.afAuth.auth.signInWithPopup(provider);
+        log.then(res=>{this.username='This is the result:' + JSON.stringify(res)})
+        .catch(e=>{
+            this.username = 'This is the error' + JSON.stringify(e);
         })
-        
-            this.afAuth.auth.signInWithCredential(firebase.auth.GoogleAuthProvider.credential(Gplus.idToken));
-            this.username = 'it worked i guess';
-    }catch{
-        this.username= 'ERROR!!'
-    }
+        // const Gplus = this.googlePlus.login({
+        //     'webClientId': '690959688562-067djvtm6sp6f9a75lu64a6fhj7m7bin.apps.googleusercontent.com',
+        //     'offline' : true,
+        //     'scopes' : 'profile email'
+        // })
+        // this.username = 'it worked i guess';
+        //     this.afAuth.auth.signInWithCredential(firebase.auth.GoogleAuthProvider.credential(Gplus.idToken));
+    
     }
 
 }
